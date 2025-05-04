4a6143c7913083cf841e14d8ad08959a0bbaea0e
vanduongcs <ddvanworkmail@gmail.com>
2025-05-04 15:04:53 +0700

    keo-tha

--- src/utils/sorts.js
+++ src/utils/sorts.js
@@ -1,1 +1,11 @@
 // Sort columns
+export const mapOrder = (originalArray, orderArray, key) => {
+  if (!originalArray || !orderArray || !key) return []
+
+  const clonedArray = [...originalArray]
+  const orderedArray = clonedArray.sort((a, b) => {
+    return orderArray.indexOf(a[key]) - orderArray.indexOf(b[key])
+  })
+
+  return orderedArray
+}
